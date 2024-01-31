937d0a30e3e96e501d0bdf89309f08cec6d00944
Xinyi-Song <xinyi.song0825@gmail.com>
2024-01-31 16:20:16 -0500

    Correct JavaScripts in array1-q.js

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -3,9 +3,27 @@ replace the username part with * characters.
 */
 const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.edu', 'jk@neu.edu', 'jsned@', 'ai_me@mugar.northeastern.edu'];
 window.onload = function hideEmail() {
+    for (const email of emails) {
+        if(email.match(/^[a-z0-9_]+@(.+\.)*northeastern.edu$/)) {
+            console.log(email);
+            const listItenm = document.createElement('li');
+            listItenm.textContent = email.slice(0,email.indexOf('@')).replace(/./g,'*') + email.slice(email.indexOf('@'));
+            list.appendChild(listItenm);
+        }
+    }
+}
+
     const list = document.getElementById('emails')
     list.innerHTML = '';
     for (const email of emails) {
         // complete the loop
+        if(isValid(email)) {
+            //splite based on @
+            //split[0].replace().
+        }
     }
+
+
+function isValid(email) {
+    return /^[a-zA-Z0-9_]/
 }
